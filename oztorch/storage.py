0a0b9fc2c001dd1e4acb90a756847922b945de65
ozaner <56015962+ozaner@users.noreply.github.com>
2022-11-10 00:37:18 -0500

    Create Storage class
    - Created Storage class which encapsulates a Tensor's size, strides, and data.
    - Added a print method to Tensor.
    - Added single value indexing to Tensors.

---
+++ oztorch/storage.py
@@ -0,0 +1,106 @@
+from typing import Optional
+
+from math import prod
+
+from .dtype import dtype, default_dtype_list
+from .commonTyping import IntTuple, ValidPyType
+
+class Storage():
+  def __init__(self, data: list[ValidPyType], size: IntTuple, /, stride: Optional[IntTuple] = None, dtype: Optional[dtype] = None):
+    # set backing fields
+    self.size = size
+    self.stride = stride or _calculate_standard_stride(size)
+    self.data = data
+
+    #cast storage to dtype (or minimal dtype)
+    _cast_list_in_place(self.data, dtype)
+
+  # Returns number elements in the logical tensor (not the underlying storage, i.e. shape matters)
+  def numel(self) -> int:
+    return prod(self.size)
+
+  def index_to_raw(self, index: IntTuple) -> int:
+    return sum(i*j for i,j in zip(index, self.stride))
+
+  def _preprocess_indexing(self, key: IntTuple | int) -> IntTuple:
+    # cast single int to IntTuple
+    if isinstance(key, int):
+      key = (key,)
+
+    # check if key is valid
+    if len(key) != len(self.size):
+      raise Exception("Tensors currently only support indexing individual elements.")
+
+    return key
+
+  def __getitem__(self, key: IntTuple | int) -> ValidPyType:
+    key = self._preprocess_indexing(key)
+    return self.data[self.index_to_raw(key)]
+
+  def __setitem__(self, key: IntTuple | int, val: ValidPyType):
+    key = self._preprocess_indexing(key)
+    self.data[self.index_to_raw(key)] = val
+
+  def to_string(self) -> str:
+    s = ""
+    for raw_index in range(self.numel()):
+      index = _contiguous_raw_to_index(raw_index, self.size)
+      l = ""
+      for i in range(len(index) - 1, -1, -1):
+        if index[i] == 0:
+          l = "\n%s[" % ("\t" * i) + l
+        else:
+          break
+      s += l
+      v = self[index]
+      s += f"{v}"
+      l = ""
+      for i in range(len(index) - 1, -1, -1):
+        if index[i] == self.size[i] - 1:
+          l += "]"
+        else:
+          break
+      if l:
+        s += l
+      else:
+        s += " "
+    return f"tensor({s})"
+
+
+### -----------------------------
+## Private methods
+### -----------------------------
+def _calculate_standard_stride(size: IntTuple) -> IntTuple:
+  ls = [0]*len(size)
+  product = 1
+  for i, s in reversed(list(enumerate(size))):
+    ls[i] = product
+    product*=s
+  return tuple(ls)
+
+def _cast_list_in_place(storage: list[ValidPyType], dtype: Optional[dtype]):
+  # determine minimal numeric type
+  if dtype is None:
+    highest_rank = -1
+    for i in range(len(storage)):
+      if isinstance(storage[i], bool) and highest_rank < 0:
+        highest_rank = 0
+      elif isinstance(storage[i], int) and highest_rank < 1:
+        highest_rank = 1
+      elif isinstance(storage[i], float):
+        highest_rank = 2
+      
+    if highest_rank == -1:
+      highest_rank = 2 #default is float64
+    dtype = default_dtype_list[highest_rank]
+  
+  # cast all values
+  for i in range(len(storage)):
+    storage[i] = dtype.cast_fn(storage[i])
+
+def _contiguous_raw_to_index(ordinal: int, size: IntTuple) -> IntTuple:
+  output: list[int] = [0]*len(size)
+  for i, base in reversed(list(enumerate(size))):
+      output[i] = ordinal % base
+      ordinal = ordinal // base
+  return tuple(output)
--- oztorch/tensor.py
+++ oztorch/tensor.py
@@ -4,7 +4,7 @@ from .dtype import dtype
 
 from math import prod
 
-import oztorch
+from .storage import Storage
 from .autograd import History, Variable
 
 
@@ -12,17 +12,13 @@ from .autograd import History, Variable
 ## Tensor class
 ### -----------------------------
 class Tensor(Variable):
-  def __init__(self, storage: list[ValidPyType], size: IntTuple, /, stride: Optional[IntTuple] = None, dtype: Optional[dtype] = None, requires_grad: bool = False, history: Optional[History] = None):
+  def __init__(self, storage: Storage, /, requires_grad: bool = False, history: Optional[History] = None):
     # Variable stuff
     super().__init__(requires_grad, history)
 
-    # set backing fields    
-    self._size = size
-    self._stride = stride or _calculate_standard_strides(size)
-    self._storage = storage
+    # init backing storage
+    self.storage = storage
 
-    #cast storage to dtype (or minimal dtype)
-    _cast_list_in_place(self._storage, dtype)
 
   ## Size method & overloads ------------------
   @overload
@@ -48,9 +44,9 @@ class Tensor(Variable):
 
   def size(self, dim: Optional[int] = None) -> IntTuple | int:
     if dim is None:
-      return self._size
+      return self.storage.size
     else:
-      return self._size[dim]
+      return self.storage.size[dim]
   ## ---------------------------------------
 
   def dim(self) -> int:
@@ -68,11 +64,20 @@ class Tensor(Variable):
 
     PyTorch equivalent: https://pytorch.org/docs/stable/generated/torch.numel.html
     """
-    return prod(self.size())
+    return self.storage.numel()
 
   # Returns the strides of the tensor
   def stride(self) -> IntTuple:
-    return self._stride
+    return self.storage.stride
+  
+  def __getitem__(self, key: IntTuple | int) -> ValidPyType:
+    return self.storage[key]
+
+  def __setitem__(self, key: IntTuple | int, val: ValidPyType):
+    self.storage[key] = val
+
+  def __repr__(self) -> str:
+    return self.storage.to_string()
 
 
   ### -----------------------------
@@ -83,20 +88,12 @@ class Tensor(Variable):
     Creates a copy of this `Tensor` (with the same underlying storage)
     without a history, and with `requires_grad` set to `False`
     """
-    return Tensor(self._storage, self._size, stride=self._stride, requires_grad=requires_grad, history=history)
+    return Tensor(self.storage, requires_grad=requires_grad, history=history)
 
 
 ### -----------------------------
 ## Private methods
 ### -----------------------------
-def _calculate_standard_strides(size: IntTuple) -> IntTuple:
-  ls = [1]
-  product = 1
-  for i in range(len(size)):
-    product*=size[i]
-    ls.insert(0, product)
-  return tuple(ls)
-
 def _clean_PyTensor_helper(data: Sequence | ValidPyType, dim: int, sizes: list) -> list[ValidPyType]:
   if isinstance(data, Sequence):
     size = len(data)
@@ -117,32 +114,12 @@ def _clean_PyTensor_helper(data: Sequence | ValidPyType, dim: int, sizes: list) 
   else:
     return [data]
 
-def _clean_pyTensor(data: Sequence | ValidPyType) -> tuple[list[ValidPyType], IntTuple]:
+def _clean_PyTensor(data: Sequence | ValidPyType) -> tuple[list[ValidPyType], IntTuple]:
   size = []
   flattened_data = _clean_PyTensor_helper(data, 0, size)
   if size[-1] == -1:
     size.pop() #remove single element psuedo-dimension
   return flattened_data, tuple(size)
-
-def _cast_list_in_place(storage: list[ValidPyType], dtype: Optional[dtype]):
-  # determine minimal numeric type
-  if dtype is None:
-    highest_rank = -1
-    for i in range(len(storage)):
-      if isinstance(storage[i], bool) and highest_rank < 0:
-        highest_rank = 0
-      elif isinstance(storage[i], int) and highest_rank < 1:
-        highest_rank = 1
-      else: #must be float
-        highest_rank = 2
-    
-    if highest_rank == -1:
-      highest_rank = 2 #default is float64
-    dtype = oztorch.default_dtype_list[highest_rank]
-  
-  # cast all values
-  for i in range(len(storage)):
-    storage[i] = dtype.cast_fn(storage[i])
 
 
 ### -----------------------------
@@ -150,14 +127,13 @@ def _cast_list_in_place(storage: list[ValidPyType], dtype: Optional[dtype]):
 ### -----------------------------
 #creates a Tensor using an n-dim python sequence (of valid dtypes)
 def tensor(data: Sequence | ValidPyType, /, dtype: Optional[dtype] = None, requires_grad: bool = False) -> Tensor:
-  flattened_data, size = _clean_pyTensor(data)
-  return Tensor(flattened_data, size, dtype=dtype, requires_grad = requires_grad)
+  flattened_data, size = _clean_PyTensor(data)
+  return Tensor(Storage(flattened_data, size, dtype=dtype), requires_grad = requires_grad)
 
-# Returns a tensor of full zeros of given size
 def full(size: IntTuple, fill_value: ValidPyType, /, dtype: Optional[dtype] = None, requires_grad: bool = False) -> Tensor:
   numel = prod(size)
   data = [fill_value]*numel
-  return Tensor(data, size, dtype=dtype, requires_grad = requires_grad)
+  return Tensor(Storage(data, size, dtype=dtype), requires_grad = requires_grad)
 
 def zeros(size: IntTuple, /, dtype: Optional[dtype] = None, requires_grad: bool = False) -> Tensor:
   return full(size, 0.0, dtype=dtype, requires_grad = requires_grad)
