ea75603a8acc5d17684e0618165607e3f5524de8
ozaner <56015962+ozaner@users.noreply.github.com>
2022-11-10 00:36:48 -0500

    Move Function class to autograd module

--- oztorch/__init__.py
+++ oztorch/__init__.py
@@ -1,6 +1,5 @@
 from .tensor import *
 from .dtype import *
 from .autograd import *
-from .function import *
 
-__all__ = ["dtype", "tensor", "autograd", "function"]
+__all__ = ["dtype", "tensor", "autograd"]
--- oztorch/autograd.py
+++ oztorch/autograd.py
@@ -1,7 +1,4 @@
 from typing import Generic, Optional, Sequence, TypeVar, Self
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-  from .function import Function
 
 from dataclasses import dataclass
 from abc import abstractmethod
@@ -76,3 +73,41 @@ class History(Generic[T]):
   grad_fn: Optional[type['Function[T]']] = None
   ctx: Optional[Context[T]] = None
   inputs: Sequence[T] = ()
+
+
+class Function(Generic[T]):
+  @classmethod
+  @abstractmethod
+  def forward(cls, ctx: Context, *inps: T) -> T:
+    ...
+
+  @classmethod
+  @abstractmethod
+  def backward(cls, ctx: Context, *inps: T) -> tuple[T, ...]:
+    ...
+
+  @classmethod
+  def apply(cls, *vals: T) -> T:
+
+    # determines if this function call needs to be added to the graph
+    # i.e. if any of its inputs requires grad
+    need_grad = any([v.requires_grad for v in vals])
+
+    # Create the context.
+    # - Specify no_grad so that forward pass can skip saving if unneeded
+    ctx = Context(no_grad = not need_grad)
+
+    # if we didn't detach the vals before calling forward,
+    # then the computation graph would include the intermediate function calls inside `forward`,
+    # even though `backward` should already account for these
+    clean_vals = [v.detach() for v in vals]
+
+    # Call forward with the detached inputs.
+    out = cls.forward(ctx, *clean_vals)
+
+    # Initalize history, if need_grad.
+    # Else leave blank (tensor will be a leaf, unknowing of its history)
+    history = History(cls, ctx, vals) if need_grad else None
+
+    # Create a new tensor from the out and history.
+    return out._copy_wo_grad_info(need_grad, history)
